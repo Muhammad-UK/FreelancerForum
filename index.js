031dab80880ed550169b1cc18ff334c30490d844
Muhammad-UK <muhammadkhan3655@gmail.com>
2023-12-04 21:49:23 -0600

    added function to list freelancers to the html

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+const createListFromObject = (object) => {
+  const listed = object
+    .map((value) => {
+      return `
+      <section>
+      <p>${value.name}</p>
+      <p>Hourly Rate: ${value.price}</p>
+      <p>Occupation: ${value.occupation}</p>
+      </section>
+    `;
+    })
+    .join("");
+  return listed;
+};
+
 const spanCount = document.querySelector("#spanCount");
 const averageRate = document.querySelector("#averageRate");
+const listingSection = document.querySelector("#listingSection");
 
 const names = [
   "Dr. Slice",
@@ -26,3 +42,5 @@ freelancers.forEach((value) => {
   sum += value.price;
 });
 averageRate.innerHTML = sum / freelancers.length;
+
+listingSection.innerHTML = createListFromObject(freelancers);
