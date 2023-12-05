167d721fcb5f384705311208a5fed47571acfd68
Muhammad-UK <muhammadkhan3655@gmail.com>
2023-12-04 23:30:33 -0600

    added comments to js code and attempted some css

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const createListFromObject = (object) => {
-  const listed = object
+const createListFromArrayofObjects = (arr) => {
+  //main function that adds html tags to the array
+  //making use of "toFixed" to get 2 decimal points for prices to match the average rate
+  const listed = arr
     .map((value) => {
       return `
       <section>
@@ -8,18 +10,21 @@ const createListFromObject = (object) => {
       <p>Occupation: ${value.occupation}</p>
       </section>
     `;
+      //using sections as well to attempt to "css-proof" the function
     })
     .join("");
   return listed;
 };
 
 const createRandomFreelancersObject = (names, occupations) => {
+  //this function creates and object that is pushable to the freelancer array
   const getRandomIndex = (arr) => {
+    //defining this as a function so that I can call it multiple times for different arrays
     return Math.floor(Math.random() * arr.length);
   };
-  const randomPrice = Math.floor(Math.random() * (100 - 15 + 1) + 15);
+  const randomPrice = Math.floor(Math.random() * (100 - 15 + 1) + 15); //using "100" and "15" as max and min values for the hourly rates random prices
   const randomObject = {
-    name: `${names[getRandomIndex(names)]}`,
+    name: `${names[getRandomIndex(names)]}`, //using template literals because the value getting returned should be a string
     price: randomPrice,
     occupation: `${occupations[getRandomIndex(occupations)]}`,
   };
@@ -27,6 +32,7 @@ const createRandomFreelancersObject = (names, occupations) => {
 };
 
 const render = () => {
+  //this function needs to be called multiple times due to the interval to update the website with new data as it's received
   spanCount.innerHTML = freelancers.length;
 
   let sum = 0;
@@ -35,9 +41,10 @@ const render = () => {
   });
   averageRate.innerHTML = (sum / freelancers.length).toFixed(2);
 
-  listingSection.innerHTML = createListFromObject(freelancers);
+  listingSection.innerHTML = createListFromArrayofObjects(freelancers);
 };
 
+//created these id's in the HTML so I can select them and modify them here
 const spanCount = document.querySelector("#spanCount");
 const averageRate = document.querySelector("#averageRate");
 const listingSection = document.querySelector("#listingSection");
@@ -61,6 +68,7 @@ const freelancers = [
 ];
 
 render();
+//calling the render() function before the setInterval starts in order to always display the first two objects before adding anymore
 
 const interval = setInterval(() => {
   freelancers.push(createRandomFreelancersObject(names, occupations));
@@ -68,4 +76,5 @@ const interval = setInterval(() => {
   if (freelancers.length === 15) {
     clearInterval(interval);
   }
+  //stopping the interval after 15 freelancers because otherwise it would go on forever which I can't imagine would be great for memory usage
 }, 2500);
